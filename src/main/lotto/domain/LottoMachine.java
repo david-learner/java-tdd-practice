0b803cff59b163aaeabb4851d73386f5217eb456
David <xodnjs2549@naver.com>
2018-05-02 11:23:03 +0900

    Feat : getLotto Test 추가

--- src/main/lotto/domain/LottoMachine.java
+++ src/main/lotto/domain/LottoMachine.java
@@ -1,5 +1,8 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class LottoMachine {
@@ -11,5 +14,13 @@ public class LottoMachine {
 
     public static int generateRandomNo() {
         return ThreadLocalRandom.current().nextInt(1, 46);
+    }
+
+    public static List<Integer> getLotto() {
+        List<Integer> lotto = new ArrayList<>();
+        while (lotto.size() < 6) {
+            lotto.add(generateRandomNo());
+        }
+        return lotto;
     }
 }
--- src/test/lotto/domain/LottoMachineTest.java
+++ src/test/lotto/domain/LottoMachineTest.java
@@ -2,6 +2,9 @@ package domain;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -14,5 +17,12 @@ public class LottoMachineTest {
     @Test
     public void randomNoGenerate() {
         System.out.println(LottoMachine.generateRandomNo());
+    }
+
+    @Test
+    public void getLotto() {
+        List<Integer> lotto = LottoMachine.getLotto();
+        assertThat(lotto.size() ,is(6));
+        System.out.println(Arrays.toString(lotto.toArray()));
     }
 }
