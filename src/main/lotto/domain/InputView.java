e5782b6d11a100c34d48efa589e89aa504aa6ca3
David <xodnjs2549@naver.com>
2018-05-02 10:39:16 +0900

    Feat : InputView getCost 테스트 추가

---
+++ src/main/lotto/domain/InputView.java
@@ -0,0 +1,9 @@
+package domain;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static int getCost(Scanner scanner) {
+        return scanner.nextInt();
+    }
+}
--- src/test/lotto/domain/InputViewTest.java
+++ src/test/lotto/domain/InputViewTest.java
@@ -1,10 +1,23 @@
 package domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class InputViewTest {
-    @Test
-    public void nothing() {
+import java.util.Scanner;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class InputViewTest {
+    private Scanner scanner;
+
+    @Before
+    public void setUp() {
+        scanner = new Scanner("12000");
+    }
+
+    @Test
+    public void getCost() {
+        assertThat(InputView.getCost(scanner),is(12000));
     }
 }
